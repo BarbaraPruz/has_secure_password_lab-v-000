3342f54d21030a7430ea1f4a969f99535be9b1f1
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-04 05:01:14 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,6 @@ class UsersController < ApplicationController
   def create
     user = User.new(user_params).save
     redirect_to(controller: 'sessions', action: 'create', user_params) 
-    redirect_to homepage_path
   end
 
   def homepage
