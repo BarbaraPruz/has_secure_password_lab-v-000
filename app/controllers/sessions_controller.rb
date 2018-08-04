0187c4e2c2bdc3a2a89caac333b49c70cd90108b
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-08-04 04:48:05 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,9 @@ class SessionsController < ApplicationController
   end
 
   def create
-    binding.pry
     if !params[:user][:name].present?
       redirect_to login_path
     else
-      binding.pry
       session[:user_id] = User.find_by(:name => params[:user][:name]).id
       redirect_to homepage_path
     end
